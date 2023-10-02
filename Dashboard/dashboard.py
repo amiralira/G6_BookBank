f4634a84f1fb76c4fda8355fd54cd52d65ed92a2
w3schools-test <test@w3schools.com>
2023-10-02 19:33:29 +0330

    Hi

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -21,6 +21,50 @@ book_tags = pd.read_sql('SELECT * FROM BookTags', conn)#column names: index	Book
 tags = pd.read_sql('SELECT * FROM Tags', conn)#column names:   	index	ID	Name	URL
 persons = pd.read_sql('SELECT * FROM Persons', conn)#column names:  	index	ID	Name	URL
 
+
+
+
+def get_scatter_data():
+    query = '''
+    SELECT b.TitleRating AS BookScore, p.Price
+    FROM Books AS b
+    INNER JOIN Price AS p ON b.ID = p.BookID
+    WHERE b.TitleRating IS NOT NULL AND p.Price IS NOT NULL
+    '''
+    return pd.read_sql_query(query, conn)
+
+
+def get_scatter_data():
+    query = '''
+    SELECT PageCount AS number_of_pages,
+           PersianPublishYear AS Persian_Publish_Year
+    FROM Books
+    WHERE Persian_Publish_Year != -1
+    '''
+    return pd.read_sql_query(query, conn)
+
+def get_top_translators():
+    query = '''
+    SELECT p.Name AS Translator, COUNT(bt.BookID) AS NumberOfBooksTranslated
+    FROM Persons AS p
+    INNER JOIN BookTranslators AS bt ON p.ID = bt.PersonID
+    GROUP BY Translator
+    ORDER BY NumberOfBooksTranslated DESC
+    LIMIT 10;
+    '''
+    return pd.read_sql_query(query, conn)
+def get_top_authors():
+    query = '''
+    SELECT p.Name AS Author, COUNT(ba.BookID) AS NumberOfBooks
+    FROM Persons AS p
+    INNER JOIN BookAuthors AS ba ON p.ID = ba.PersonID
+    GROUP BY Author
+    ORDER BY NumberOfBooks DESC
+    LIMIT 10;
+    '''
+    return pd.read_sql_query(query, conn)
+
+
 def get_books_by_year():
     query = '''
     SELECT GregorianPublishYear AS Year, COUNT(*) AS NumberOfBooks
@@ -87,15 +131,15 @@ fig.update_layout(xaxis_title='Publisher', yaxis_title='Number of Books/Publicat
 st.plotly_chart(fig)
 
 # Number of books/publications chart
-st.subheader('Number of Publications by Type')
+st.subheader('Number of Publications by GhatE')
 
-st.subheader('Number of Books/Publications by Type')
+st.subheader('Number of Books/Publications by GhatE')
 fig_books = px.bar(
-    books['CoverType'].value_counts(),
-    x=books['CoverType'].value_counts().index,
-    y=books['CoverType'].value_counts().values,
-    labels={'x': 'Cover Type', 'y': 'Number of Books'},
-    title='Number of Books/Publications by Type'
+    books['GhatE'].value_counts(),
+    x=books['GhatE'].value_counts().index,
+    y=books['GhatE'].value_counts().values,
+    labels={'x': 'GhatE', 'y': 'Number of Books'},
+    title='Number of Books/Publications by GhatE'
 )
 st.plotly_chart(fig_books)
 
@@ -113,6 +157,89 @@ if not books_by_year.empty:
     st.plotly_chart(fig_years)
 else:
     st.info("No data available for the 'Number of Books by Year of Publication' chart.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Get the top 10 authors
+
+top_authors = get_top_authors()
+
+# Display the top 10 authors and the number of books they've authored
+if not top_authors.empty:
+    st.subheader('Top 10 Authors by Number of Books Authored')
+    fig_authors = px.bar(
+        top_authors,
+        x='NumberOfBooks',
+        y='Author',
+        orientation='h',
+        labels={'NumberOfBooks': 'Number of Books', 'Author': 'Author Name'},
+        title='Top 10 Authors by Number of Books Authored'
+    )
+    st.plotly_chart(fig_authors)
+else:
+    st.info("No data available for the 'Top Authors by Number of Books Authored' chart.")
+
+
+
+
+
+
+
+# Get the top 10 translators
+top_translators = get_top_translators()
+
+# Display the top 10 translators and the number of books they've translated
+if not top_translators.empty:
+    st.subheader('Top 10 Translators by Number of Books Translated')
+    fig_translators = px.bar(
+        top_translators,
+        x='NumberOfBooksTranslated',
+        y='Translator',
+        orientation='h',
+        labels={'NumberOfBooksTranslated': 'Number of Books Translated', 'Translator': 'Translator Name'},
+        title='Top 10 Translators by Number of Books Translated'
+    )
+    st.plotly_chart(fig_translators)
+else:
+    st.info("No data available for the 'Top Translators by Number of Books Translated' chart.")
+
+
+
+# Get the data for the scatter chart
+scatter_data = get_scatter_data()
+
+# Display the scatter chart
+if not scatter_data.empty:
+    st.subheader('Scatter Chart: Number of Pages vs. Persian Publish Year')
+    fig_scatter = px.scatter(
+        scatter_data,
+        x='number_of_pages',
+        y='Persian_Publish_Year',
+        labels={'number_of_pages': 'Number of Pages', 'Persian_Publish_Year': 'Persian Publish Year'},
+        title='Scatter Chart: Number of Pages vs. Persian Publish Year'
+    )
+    st.plotly_chart(fig_scatter)
+else:
+    st.info("No data available for the scatter chart.")
+
+
+# # Merge the books and price tables
+# books_price = pd.merge(books, price, left_on='ID', right_on='BookID')[['ID', 'BookID', 'EnglishTitle', 'Price', 'TitleRating','Title','PageCount']]
+# # Create a scatter chart of book score vs. price
+# fig = px.scatter(books_price, x='TitleRating', y='Price', color='Title', size='PageCount', hover_name='Title')
+
+# # Display the scatter chart
+# st.plotly_chart(fig)
 
 
 
