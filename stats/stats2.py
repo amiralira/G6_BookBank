0a463161e8b93388df4d59851b925df00e5bb3d7
Taratajdani <116340000+Taratajdani@users.noreply.github.com>
2023-10-03 10:51:48 +0330

    Update stats2.py

--- stats/stats2.py
+++ stats/stats2.py
@@ -7,12 +7,15 @@ df_translated = pd.read_csv('translator.csv')
 df = pd.read_csv('books.csv')
 
 #books that are translated
+
 merged_df = pd.merge(df_books, df_translated, on ='BookID')
 
 #books that are not translated
+
 filtered_df = df_books[~df_books['BookID'].isin(df_translated['BookID'])]
 
 #ttest for hypothesis 1 :
+
 translated_books = merged_df['Price']
 non_translated_books = filtered_df['Price']
 t1 = stats.ttest_ind(translated_books, non_translated_books)
@@ -30,7 +33,13 @@ merged_hard = merged_h[merged_h['Title'].isin(merged_s['Title'])]
 merged_soft = merged_s[merged_s['Title'].isin(merged_h['Title'])]
 
 #ttest for hypothesis 2 :
+
 hard_price = merged_hard['Price']
 soft_price = merged_soft['Price']
 t2 = stats.ttest_ind(hard_price, soft_price)
+
+# مقدار p-value بسیار کوچکتر از سطح معناداری 0.05 است. بنابراین، می‌توان نتیجه گرفت که نوع جلد تأثیر قابل توجه‌ای در قیمت کتاب دارد.
+# 1. ممکن است در بازار کتاب، جلد سخت به عنوان یک عامل ارزشمند شناخته شده است و ممکن است خریداران بیشتری آماده باشند برای پرداخت قیمت بالاتری برای کتاب‌های با جلد سخت.
+# 2. توجیه دیگر برای تفاوت قیمت بین کتاب‌های با جلد سخت و نرم این است که جلد سخت معمولاً هزینه تولید بیشتری دارد. به عنوان مثال، استفاده از جلد سخت ممکن است نیاز به استفاده از مواد با کیفیت بالاتر و فرآیندهای تولید پیچیده‌تری داشته باشد که هزینه بیشتری را ایجاد می‌کند. بنابراین، این هزینه بالاتر می‌تواند به قیمت بالاتر کتاب‌های با جلد سخت منجر شود.
+# 3. یک توجیه دیگر ممکن است در ارتباط با مقاومت و طول عمر جلد سخت باشد. جلد سخت معمولاً مقاومت بیشتری در برابر سایش، شکستگی و آسیب‌های دیگر دارد. این مقاومت بالاتر می‌تواند منجر به طول عمر بیشتر کتاب‌های با جلد سخت شود و خریداران ممکن است تمایل داشته باشند برای خرید کتابی که مقاومت و طول عمر بیشتری دارد، قیمت بالاتری را پرداخت کنند.
 
