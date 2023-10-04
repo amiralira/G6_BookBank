92d1065b5ca3d267130143c7760d82d7230e3a6e
NimNimEs <nimaesmailee82@gmail.com>
2023-10-04 13:49:04 +0330

    stats1 v2

--- stats/stats1.py
+++ stats/stats1.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import sqlite3
 
-import sqlite3
-
 try:
     conn = sqlite3.connect('my_books.db')
 except sqlite3.Error as error:
@@ -10,7 +8,7 @@ except sqlite3.Error as error:
 
 cursor = conn.cursor()
 
-#1st client request
+#1st client's request
 query = 'SELECT DISTINCT\
         p.Name AS name \
     FROM BookTags AS bt \
@@ -22,58 +20,122 @@ query = 'SELECT DISTINCT\
         b.ID = ba.BookID \
     INNER JOIN Persons AS p ON \
         ba.PersonID = p.ID \
-    WHERE t.Name LIKE "%عاشقانه%" \
+    WHERE t.Name LIKE "%عاشقانه%"  AND b.DataRating >= 0 AND b.DataRating <= 5\
     ORDER BY SUM(b.DataRating) OVER(PARTITION BY P.Name) DESC \
     LIMIT 5;'
 
+
+# query = '''SELECT DISTINCT
+#         p.Name AS name 
+#     FROM BookTags AS bt 
+#     INNER JOIN Tags AS t ON 
+#         bt.BookID = t.ID 
+#     INNER JOIN Books AS B ON 
+#         bt.BookID = b.ID 
+#     INNER JOIN BookAuthors AS ba ON 
+#         b.ID = ba.BookID 
+#     INNER JOIN Persons AS p ON 
+#         ba.PersonID = p.ID 
+#     WHERE t.Name LIKE "%عاشقانه%"  AND b.DataRating >= 0 AND b.DataRating <= 5
+#     ORDER BY SUM((b.DataRating-3)*SQRT(b.Series)) OVER(PARTITION BY P.Name) DESC 
+#     LIMIT 5;'''
+
 df1 = pd.read_sql(query, conn)
+
+# 1. سومونک کید
+# 2. جوزف بویدن
+# 3. لسلی کانر
+# 4. ژوزه ساراماگو
+# 5. پیر پژو
+
+
+# sum of data rating is not a very good criterion, sum of (data rating - 3) * sqrt(series) could be a better criterion which leads to a different result
+# 1. سومونک کید
+# 2. لسلی کانر
+# 3. جوزف بویدن
+# 4. ژوزه ساراماگو
+# 5. هرتا مولر
+
 
 df1.to_csv('first_client_request.csv')
 
-# 2nd client request
-query = 'WITH tmp_table \
-    AS ( \
-    SELECT \
-        ID, \
-        Title title, \
-        NTILE(4) OVER (ORDER BY DataRating DESC) rate_quartile, \
-        DataRating rating, \
-        Price price, \
-        NTILE(5) OVER (ORDER BY Price) price_ntile \
-    FROM Books \
-    INNER JOIN Price ON \
-        Books.ID = Price.BookID \
-    ) \
-SELECT \
-    title, \
-    rating, \
-    price \
-FROM tmp_table \
-WHERE rate_quartile = 1 AND price_ntile = 1;'
+# 2nd client's request
+query = '''WITH tmp_table 
+    AS ( 
+    SELECT 
+        ID, 
+        Title title, 
+        NTILE(4) OVER (ORDER BY DataRating DESC) rate_quartile, 
+        DataRating rating, 
+        Price price, 
+        NTILE(5) OVER (ORDER BY Price) price_ntile 
+    FROM Books 
+    INNER JOIN Price ON 
+        Books.ID = Price.BookID 
+    WHERE Price."Exists" = 1
+    ) 
+SELECT 
+    title, 
+    rating, 
+    price 
+FROM tmp_table 
+WHERE rate_quartile = 1 AND price_ntile = 1 ;'''
 
 df2 = pd.read_sql(query, conn)
 
 df2.to_csv('second_client_request.csv')
 
-# Writer request
-query = 'SELECT \
-    p.ID publisher_id, \
-    p.Name publisher_name, \
-    SUM(b.DataRating) sum \
-FROM Tags t \
-INNER JOIN BookTags bt ON \
-    t.ID = bt.TagID \
-INNER JOIN Books b ON \
-    bt.BookID =b.ID \
-INNER JOIN BookPublishers bp ON \
-    bt.BookID = bp.BookID \
-INNER JOIN Publishers P ON \
-    bp.PublisherID = P.ID \
-WHERE t.Name LIKE "%تاریخ%" \
-GROUP BY p.ID \
-ORDER BY SUM(b.DataRating) DESC \
-LIMIT 5;'
+# Writer's request
+query = '''SELECT 
+    p.ID publisher_id, 
+    p.Name publisher_name, 
+    SUM(b.DataRating) sum 
+FROM Tags t 
+INNER JOIN BookTags bt ON 
+    t.ID = bt.TagID 
+INNER JOIN Books b ON 
+    bt.BookID =b.ID 
+INNER JOIN BookPublishers bp ON 
+    bt.BookID = bp.BookID 
+INNER JOIN Publishers P ON 
+    bp.PublisherID = P.ID 
+WHERE t.Name LIKE "%تاریخ%" 
+GROUP BY p.ID 
+ORDER BY SUM(b.DataRating) DESC 
+LIMIT 5;'''
+
+# query = '''SELECT 
+#     p.ID publisher_id, 
+#     p.Name publisher_name, 
+#     SUM(b.DataRating) sum 
+# FROM Tags t 
+# INNER JOIN BookTags bt ON 
+#     t.ID = bt.TagID 
+# INNER JOIN Books b ON 
+#     bt.BookID =b.ID 
+# INNER JOIN BookPublishers bp ON 
+#     bt.BookID = bp.BookID 
+# INNER JOIN Publishers P ON 
+#     bp.PublisherID = P.ID 
+# WHERE t.Name LIKE "%تاریخ%" 
+# GROUP BY p.ID 
+# ORDER BY SUM((b.DataRating-3)*SQRT(b.Series)) DESC 
+# LIMIT 5;'''
 
 df3 = pd.read_sql(query, conn)
+
+# 1. ققنوس
+# 2. علم
+# 3. نگاه
+# 4. علمی و فرهنگی
+# 5. سوره مهر
+
+# sum of data rating is not a very good criterion, sum of (data rating - 3) * sqrt(series) could be a better criterion which leads to a different result
+# 1. ققنوس
+# 2. نگاه
+# 3. نشر نی
+# 4. نشر چشمه
+# 5. علمی و فرهنگی
+
 
 df3.to_csv('writer_request.csv')
