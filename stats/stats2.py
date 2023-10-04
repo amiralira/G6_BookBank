49e7a0b9a829d14e2dda0758698df200859cd129
Taratajdani <116340000+Taratajdani@users.noreply.github.com>
2023-10-04 18:32:59 +0330

    Add files via upload

--- stats/stats2.py
+++ stats/stats2.py
@@ -7,15 +7,12 @@ df_translated = pd.read_csv('translator.csv')
 df = pd.read_csv('books.csv')
 
 #books that are translated
-
 merged_df = pd.merge(df_books, df_translated, on ='BookID')
 
 #books that are not translated
-
 filtered_df = df_books[~df_books['BookID'].isin(df_translated['BookID'])]
 
 #ttest for hypothesis 1 :
-
 translated_books = merged_df['Price']
 non_translated_books = filtered_df['Price']
 t1 = stats.ttest_ind(translated_books, non_translated_books)
@@ -24,8 +21,8 @@ df['CoverType'] = df['CoverType'].apply(str)
 df['Title'] = df['Title'].apply(str)
 df_hard = df[df['CoverType'].str.contains('جلد سخت')]
 df_soft = df[df['CoverType'].str.contains('شومیز')]
-df_h = df_hard.rename(columns = {'ID' : 'BookID', 'Title' : 'hard'})
-df_s = df_soft.rename(columns = {'ID' : 'BookID', 'Title' : 'soft'})
+df_h = df_hard.rename(columns = {'ID' : 'BookID'})
+df_s = df_soft.rename(columns = {'ID' : 'BookID'})
 
 merged_h = pd.merge(df_books, df_h, on ='BookID')
 merged_s = pd.merge(df_books, df_s, on ='BookID')
@@ -33,7 +30,6 @@ merged_hard = merged_h[merged_h['Title'].isin(merged_s['Title'])]
 merged_soft = merged_s[merged_s['Title'].isin(merged_h['Title'])]
 
 #ttest for hypothesis 2 :
-
 hard_price = merged_hard['Price']
 soft_price = merged_soft['Price']
 t2 = stats.ttest_ind(hard_price, soft_price)
@@ -43,3 +39,4 @@ t2 = stats.ttest_ind(hard_price, soft_price)
 # 2. توجیه دیگر برای تفاوت قیمت بین کتاب‌های با جلد سخت و نرم این است که جلد سخت معمولاً هزینه تولید بیشتری دارد. به عنوان مثال، استفاده از جلد سخت ممکن است نیاز به استفاده از مواد با کیفیت بالاتر و فرآیندهای تولید پیچیده‌تری داشته باشد که هزینه بیشتری را ایجاد می‌کند. بنابراین، این هزینه بالاتر می‌تواند به قیمت بالاتر کتاب‌های با جلد سخت منجر شود.
 # 3. یک توجیه دیگر ممکن است در ارتباط با مقاومت و طول عمر جلد سخت باشد. جلد سخت معمولاً مقاومت بیشتری در برابر سایش، شکستگی و آسیب‌های دیگر دارد. این مقاومت بالاتر می‌تواند منجر به طول عمر بیشتر کتاب‌های با جلد سخت شود و خریداران ممکن است تمایل داشته باشند برای خرید کتابی که مقاومت و طول عمر بیشتری دارد، قیمت بالاتری را پرداخت کنند.
 
+print(t1, t2)
